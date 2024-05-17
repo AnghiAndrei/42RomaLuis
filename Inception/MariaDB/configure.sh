84c1a0bc8c77024a38d8a3916ae782e4fe3f94ed
Andrei Anghi <aanghi@e3r4p6.42roma.it>
2024-05-17 13:38:49 +0200

    hola

--- Inception/MariaDB/configure.sh
+++ Inception/MariaDB/configure.sh
@@ -1,7 +1,7 @@
-bash
 echo 'CREATE DATABASE '$WP_DB';' > aanghi.temp
 echo 'CREATE USER '\'$WP_DB_USR\''@'\''localhost'\'' IDENTIFIED BY '\'$WP_DB_PASS\'';' >> aanghi.temp
 echo 'GRANT ALL PRIVILEGES ON '$WP_DB'.* TO '\'$WP_DB_USR\''@'\''localhost'\'';' >> aanghi.temp
 echo 'FLUSH PRIVILEGES;' >> aanghi.temp
-exit
+echo 'EXIT;' >> aanghi.temp
 mysql < aanghi.temp
+tail -f /dev/null
