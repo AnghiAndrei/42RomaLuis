a03c1b8921341c67ea5e0e408128f67898db7232
Andrei Anghi <aanghi@e3r4p6.42roma.it>
2024-05-28 17:23:49 +0200

    hola

--- Inception/MariaDB/configure.sh
+++ Inception/MariaDB/configure.sh
@@ -1,14 +1,12 @@
 # !/bin/sh
-chown -R root:root /var/lib/mysql
-mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > null.aanghi
+/usr/bin/mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 /usr/bin/mysqld --user=root --datadir=/var/lib/mysql &
 pid=$!;
 sleep 15
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS $WP_DB;"
 mysql -u root -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$WP_DB_USR'@'localhost' IDENTIFIED BY '$WP_DB_PASS';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_DB_USR'@'localhost';"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$WP_DB_USR'@'%' IDENTIFIED BY '$WP_DB_PASS';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DB.* TO '$WP_DB_USR'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 kill $pid
-sleep 15
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+exec /usr/bin/mysqld --user=root --console
