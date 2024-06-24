785c9a555068661ec29fff1f402f7371a7066c85
Andrei Anghi <aanghi@e3r4p6.42roma.it>
2024-06-24 17:55:57 +0200

    voglio mangiare il tuo pancreas

--- webserv/main.cpp
+++ webserv/main.cpp
@@ -1,14 +1,94 @@
 #include"webserv.hpp"
 
 int main(int argc, char **argv){
-    std::string conffile;
+    std::string conffile="";
     if(argc>2){
         std::cout<<"Marshal: numero di argomenti sbagliato"<<std::endl;
         return -1;
     }else if(argc==1)
         conffile="conf/default.conf";
-    else
+    else{
         conffile=argv[1];
-    std::cout<<conffile<<std::endl;
+	}
+	std::ifstream conf(conffile.c_str());
+	if(!conf.is_open()){
+        std::cout<<"Marshal: Errore nel aprire il file conf: "<<conffile<<std::endl;
+		return -1;
+	}
+	webserv webservv;
+	std::string temp;
+	bool server=false;
+	while(std::getline(conf, temp)){
+		if(temp.empty())
+			continue;
+		if(server==false && temp[0]=='{'){
+			server=true;
+			continue;
+		}
+		if(server==true && temp[0]=='}'){
+			server=false;
+			webservv.crea_server();
+			continue;
+		}
+		if(server==true && temp[0]=='{'){
+	        std::cout<<"Marshal: Errore nel file di configurazione"<<std::endl;
+			return -1;
+		}
+		if(server==false && temp[0]=='}'){
+	        std::cout<<"Marshal: Errore nel file di configurazione"<<std::endl;
+			return -1;
+		}
+	}
+	conf.close();
+	temp.clear();
+	conf.open(conffile.c_str());
+	if(!conf.is_open()){
+        std::cout<<"Marshal: Errore nel aprire il file conf: "<<conffile<<std::endl;
+		return -1;
+	}
+	server=false;
+	int serv=1;
+	while(std::getline(conf, temp)){
+		if(temp.empty())
+			continue;
+		if(server==false && temp[0]=='{'){
+			server=true;
+			continue;
+		}
+		if(server==true && temp[0]=='}'){
+			server=false;
+			serv++;
+			continue;
+		}
+		if(server==true && temp[0]=='{'){
+	        std::cout<<"Marshal: Errore nel file di configurazione"<<std::endl;
+			return -1;
+		}
+		if(server==false && temp[0]=='}'){
+	        std::cout<<"Marshal: Errore nel file di configurazione"<<std::endl;
+			return -1;
+		}
+		for (size_t i=0;i!=temp.size();i++){
+			std::string chiave;
+			std::string valore;
+			while(temp[i]!='\0' && temp[i]!=' ')
+				i++;
+			while(temp[i]!='\0' && temp[i]!='='){
+				chiave+=temp[i];
+				i++;
+			}
+			if(temp[i]!='='){
+				std::cout<<"Marshal: Errore nel file di configurazione"<<std::endl;
+				return -1;
+			}
+			while(temp[i]!='\0'){
+				valore+=temp[i];
+				i++;
+			}
+			if(temp=="server_name")
+				{;}
+		}
+		std::cout<<temp<<std::endl;
+	}
     return 0;
 }
--- webserv/webserv.hpp
+++ webserv/webserv.hpp
@@ -1,5 +1,39 @@
 #ifndef WEBSERV_HPP
 #define WEBSERV_HPP
 #include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<cstring>
+#include<sstream>
+#include<string>
+#include<vector>
 
+class webserv;
+
+class server{
+	private:
+		std::string name_server;
+	public:
+		server(webserv &master);
+
+		void set_name(std::string &copy){name_server=copy;}
+		std::string get_name(){return name_server;}
+};
+
+class webserv{
+	private:
+		std::vector<server> servers;
+	public:
+		size_t	get_n_server(){return servers.size();}
+		void	crea_server(){servers.push_back(server(*this));}
+		~webserv(){}
+		webserv(){}
+};
+
+server::server(webserv &master) {
+    std::ostringstream cy;
+    cy << master.get_n_server() + 1;
+    std::string n_server = cy.str();
+    name_server = "Server " + n_server;
+}
 #endif
