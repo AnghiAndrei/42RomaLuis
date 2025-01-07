a6696cc28f5798f945bda3184bb227ede69f1da7
Andrei Anghi <aanghi@e4r3p4.42roma.it>
2025-01-07 16:17:08 +0100

    voglio mangiare il tuo pancreas

---
+++ ft_trans/frontend/app/js/router.js
@@ -0,0 +1,155 @@
+import { loadHomePage } from '../pages/home.js';
+
+import { loadLoginPage } from '../pages/utenti/login.js';
+import { loadRegistraPage } from '../pages/utenti/registrati.js';
+import { load2faPage } from '../pages/utenti/2fa.js';
+
+import { loadGiochiHomePage } from '../pages/giochi.js';
+import { loadLocaleHomeGame1Page } from '../pages/gioco1/locale_home.js';
+import { loadLocaleGame1Page } from '../pages/gioco1/locale_gioco.js';
+import { loadCustumeserPage } from '../pages/gioco1/costumeser.js';
+
+import { loadLocaleHomeIaGame1Page } from '../pages/gioco1/locale_home_ia.js';
+import { loadLocaleIaGame1Page } from '../pages/gioco1/locale_gioco_ia.js';
+
+import { loadLocaleHomeTorneoGame1Page } from '../pages/gioco1/locale_torneo_home.js';
+import { loadLocaleTorneoGame1Page } from '../pages/gioco1/locale_torneo_gioco.js';
+import { loadLocalePreGameTorneoGame1Page } from '../pages/gioco1/locale_torneo_pregame.js';
+
+export let actualpossion='/';
+
+export let torneoclass = null;
+
+export let partitaincorso=false;
+export function setPartitaincorso(value){partitaincorso=value;}
+
+export let torneoincorso=false;
+export function setTorneoincorso(value){torneoincorso=value;}
+
+export function navigateTo(url) {
+  if(partitaincorso==true || torneoincorso==true)
+    return;
+  history.pushState(null, null, url);
+  actualpossion=url;
+  router();
+}
+
+export let activeTimers = [];
+/* activeTimers.forEach(id => clearInterval(id)); */
+
+export function router() {
+	const routes = [
+		{ path: '/', view: loadHomePage },
+		
+		{ path: '/login', view: loadLoginPage },
+		{ path: '/registrati', view: loadRegistraPage },
+		{ path: '/2fa', view: load2faPage },
+		
+		{ path: '/giochi', view: loadGiochiHomePage },
+		{ path: '/costumeser', view: loadCustumeserPage },
+		{ path: '/locale_home', view: loadLocaleHomeGame1Page },
+		{ path: '/locale_gioco', view: loadLocaleGame1Page },
+		{ path: '/locale_home_ia', view: loadLocaleHomeIaGame1Page },
+		{ path: '/locale_gioco_ia', view: loadLocaleIaGame1Page },
+		{ path: '/locale_torneo_home', view: loadLocaleHomeTorneoGame1Page },
+		{ path: '/locale_torneo_gioco', view: loadLocaleTorneoGame1Page },
+		{ path: '/locale_torneo_pregame', view: loadLocalePreGameTorneoGame1Page },
+	];
+  	const potentialMatch = routes.find((route) => location.pathname === route.path);
+ 	if (potentialMatch) {
+	  	if (document.getElementById('gameOverModal') != null) {
+		  	const modal = bootstrap.Modal.getInstance(document.getElementById('gameOverModal'));
+		if(modal) modal.hide();
+	}
+	if(potentialMatch.path!='/2fa')
+		sessionStorage.removeItem('tempjwt');
+	if((sessionStorage.getItem('jwtToken')==null && (potentialMatch.path=='/locale_torneo_pregame' || potentialMatch.path=='/locale_torneo_home' || potentialMatch.path=='/locale_home_ia' || potentialMatch.path=='/locale_gioco_ia' || potentialMatch.path=='/locale_gioco' || potentialMatch.path=='/locale_home')) || (sessionStorage.getItem('tempjwt')==null && potentialMatch.path=='/2fa')){
+		navigateTo("/");
+		return;
+    }
+
+    const navbar = document.getElementById('navbar-content');
+	const isRelevantPath = ['/locale_torneo_pregame', '/locale_gioco', '/locale_gioco_ia', '/locale_torneo_gioco'].includes(potentialMatch.path);
+	if (partitaincorso==false && isRelevantPath==true)
+		navbar.style.display = 'none';
+	else
+		navbar.style.display = 'block';
+
+	if(partitaincorso==true || torneoincorso==true)
+		navbar.style.display = 'none';
+
+	if(partitaincorso==false && torneoincorso==false)
+		potentialMatch.view();
+  	} else {
+    	if(localStorage.getItem('lingua')==null){localStorage.setItem('lingua', 'it');}
+    	import(`./../../traduzioni/${localStorage.getItem('lingua')}.js`)
+    	.then((module) => {
+      		const text = module.text;
+      		document.getElementById('main-content').innerHTML = '<div class="d-flex justify-content-center"><h2 class="text-white">'+text.p30+'</h2></div>';
+    	})
+  	}
+}
+window.addEventListener('popstate', router);
+
+
+/* NO BLOC TOUR */
+export function navigateTo2(url) {
+  if(partitaincorso==true)
+    return;
+  history.pushState(null, null, url);
+  actualpossion=url;
+  router2();
+}
+export function router2() {
+	alert("router2");
+	const routes = [
+    { path: '/', view: loadHomePage },
+    
+    { path: '/login', view: loadLoginPage },
+    { path: '/registrati', view: loadRegistraPage },
+    { path: '/2fa', view: load2faPage },
+
+    { path: '/giochi', view: loadGiochiHomePage },
+    { path: '/costumeser', view: loadCustumeserPage },
+    { path: '/locale_home', view: loadLocaleHomeGame1Page },
+    { path: '/locale_gioco', view: loadLocaleGame1Page },
+    { path: '/locale_home_ia', view: loadLocaleHomeIaGame1Page },
+    { path: '/locale_gioco_ia', view: loadLocaleIaGame1Page },
+    { path: '/locale_torneo_home', view: loadLocaleHomeTorneoGame1Page },
+    { path: '/locale_torneo_gioco', view: loadLocaleTorneoGame1Page },
+    { path: '/locale_torneo_pregame', view: loadLocalePreGameTorneoGame1Page },
+  ];
+  const potentialMatch = routes.find((route) => location.pathname === route.path);
+  if (potentialMatch) {
+    activeTimers.forEach(id => clearInterval(id));
+
+    if (document.getElementById('gameOverModal') != null) {
+      const modal = bootstrap.Modal.getInstance(document.getElementById('gameOverModal'));
+      if(modal) modal.hide();
+    }    
+
+    if(potentialMatch.path!='/2fa')
+      sessionStorage.removeItem('tempjwt');
+
+    if((sessionStorage.getItem('jwtToken')==null && (potentialMatch.path=='/locale_torneo_pregame' || potentialMatch.path=='/locale_torneo_home' || potentialMatch.path=='/locale_home_ia' || potentialMatch.path=='/locale_gioco_ia' || potentialMatch.path=='/locale_gioco' || potentialMatch.path=='/locale_home')) || (sessionStorage.getItem('tempjwt')==null && potentialMatch.path=='/2fa')){
+      navigateTo("/");
+      return;
+    }
+
+    const navbar = document.getElementById('navbar-content');
+    if ((partitaincorso==true) && (potentialMatch.path === '/locale_torneo_pregame' || potentialMatch.path === '/locale_gioco' || potentialMatch.path === '/locale_gioco_ia' || potentialMatch.path === '/locale_torneo_gioco'))
+      navbar.style.display = 'none';
+    else
+      navbar.style.display = 'block';
+
+    if(partitaincorso==true)
+      potentialMatch.view();
+  } else {
+    if(localStorage.getItem('lingua')==null){localStorage.setItem('lingua', 'it');}
+    import(`./../../traduzioni/${localStorage.getItem('lingua')}.js`)
+    .then((module) => {
+      const text = module.text;
+      document.getElementById('main-content').innerHTML = '<div class="d-flex justify-content-center"><h2 class="text-white">'+text.p30+'</h2></div>';
+    })
+  }
+}
---
+++ ft_trans/frontend/app/pages/gioco1/locale_gioco_ia.js
@@ -0,0 +1,130 @@
+import { startCountdown } from './_timer.js';
+import { PaddleIA } from './_paddle_ia.js';
+import { Paddle } from './_paddle_uman.js';
+import { Ball } from './_ball.js';
+
+function gameLoop(canvas, leftPaddle, rightPaddle, upPaddle, downPaddle, ball, gameState) {
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    leftPaddle.draw(ctx);
+    leftPaddle.update(canvas);
+  
+    rightPaddle.draw(ctx);
+    rightPaddle.update(canvas);
+
+    if(sessionStorage.getItem('nplayerg1')==3 || sessionStorage.getItem('nplayerg1')==4){
+        upPaddle.draw(ctx);
+        upPaddle.update(canvas);
+    }
+
+    if(sessionStorage.getItem('nplayerg1')==4){
+        downPaddle.draw(ctx);
+        downPaddle.update(canvas);
+    }
+
+    ball.draw(ctx);
+    ball.update(canvas, leftPaddle, rightPaddle);
+    ctx.fillStyle='white';
+    ctx.font='bold 100px Arial';
+    ctx.textAlign='center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('PONG', canvas.width / 2, canvas.height / 2);
+    ball.checkCollision(leftPaddle, rightPaddle, upPaddle, downPaddle);
+    if (gameState.gameRunning)
+        requestAnimationFrame(() => gameLoop(canvas, leftPaddle, rightPaddle, upPaddle, downPaddle, ball, gameState));
+}
+
+export function loadLocaleIaGame1Page() {
+    if(localStorage.getItem('lingua')==null){localStorage.setItem('lingua', 'it');}
+    import(`./../../traduzioni/${localStorage.getItem('lingua')}.js`)
+    .then((module) => {
+        const text = module.text;
+        const content = document.getElementById('main-content');
+        content.innerHTML = `
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h1 class="h3 mb-3 fw-normal text-white" id="testo">`+text.p22+`</h1>
+                <h2 class="h3 mb-3 fw-normal text-white" id="tempo">0 : 0</h2>
+                <canvas class="h-100 w-100" id="gameCanvas"></canvas>
+            </div>
+            <div class="modal fade" id="gameOverModal" tabindex="-1" aria-labelledby="gameOverModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content mybg">
+                        <div class="modal-header d-flex flex-column justify-content-center align-items-center">
+                            <h5 class="modal-title text-white" id="gameOverModalLabel">Game Over</h5>
+                        </div>
+                        <div class="modal-body">
+                            <h1 class="text-white" id="gameOverMessage"></h1>
+                            <p class="mt-5 mb-3">
+                                <a data-link href="/giochi" class="h4 px-2 link-secondary text-white">`+text.p31+`</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        const canvas = document.getElementById('gameCanvas');
+        const devicePixelRatio = window.devicePixelRatio || 1;
+        const width = canvas.offsetWidth * devicePixelRatio;
+        const height = canvas.offsetHeight * devicePixelRatio;
+
+        canvas.width = width;
+        canvas.height = height;
+
+        const paddleWidth = 15;
+        const paddleHeight = 220;
+        const radioball = 20;
+    
+        const ball = new Ball(canvas.width / 2, canvas.height / 2, radioball);
+        const leftPaddle = new Paddle(10, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 'w', 's', 'sg');
+        const rightPaddle = new PaddleIA(canvas.width - paddleWidth - 10, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 'sg', ball);
+        let upPaddle = null;
+        let downPaddle = null;
+        if(sessionStorage.getItem('nplayerg1')==3 || sessionStorage.getItem('nplayerg1')==4)
+            upPaddle = new PaddleIA(canvas.width / 2 - paddleHeight / 2, 10, paddleHeight, paddleWidth, 'ds', ball);
+        if(sessionStorage.getItem('nplayerg1')==4)
+            downPaddle = new PaddleIA(canvas.width / 2 - paddleHeight / 2, canvas.height - 10 - paddleWidth, paddleHeight, paddleWidth, 'ds', ball);
+    
+        const ctx = canvas.getContext('2d');
+        ctx.scale(devicePixelRatio, devicePixelRatio);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle='white';
+        ctx.font='bold 25px Arial';
+        ctx.textAlign='center';
+        ctx.textBaseline = 'middle';
+        if(sessionStorage.getItem("nplayerg1")==2 || sessionStorage.getItem("nplayerg1")==3 || sessionStorage.getItem("nplayerg1")==4){
+            ctx.fillText(sessionStorage.getItem("p1"), 40, (canvas.height / 2)- 60);
+            ctx.fillText('↑ w', 30, (canvas.height / 2)- 30);
+            ctx.fillText('↓ s', 30, canvas.height / 2);
+            ctx.fillText('BOT2', canvas.width - 100, (canvas.height / 2));
+        }
+        if(sessionStorage.getItem("nplayerg1")==3 || sessionStorage.getItem("nplayerg1")==4)
+            ctx.fillText('BOT3', canvas.width / 2, 60);
+        if(sessionStorage.getItem("nplayerg1")==4)
+            ctx.fillText('BOT4', canvas.width / 2, canvas.height - 60);
+        ctx.font='bold 100px Arial';
+        ctx.fillText('PONG', canvas.width / 2, canvas.height / 2);
+
+        document.getElementById('testo').innerHTML=text.p22;
+        let gameState = {
+            gameRunning: false,
+            gameStart: false,
+        };
+        document.addEventListener('keydown', (e) => {
+            if (e.key === ' '){
+                if (gameState.gameStart==false){
+                    gameState.gameRunning = true;
+                    gameState.gameStart = true;
+                    if(sessionStorage.getItem("nplayerg1")==2)
+                        document.getElementById('testo').innerHTML=sessionStorage.getItem('p1')+" 0 - 0 BOT1";
+                    else if(sessionStorage.getItem("nplayerg1")==3)
+                        document.getElementById('testo').innerHTML=sessionStorage.getItem('p1')+" 0 - 0 BOT1 - BOT2 0";
+                    else if(sessionStorage.getItem("nplayerg1")==4)
+                        document.getElementById('testo').innerHTML=sessionStorage.getItem('p1')+" 0 - 0 BOT1 - BOT2 0 - BOT3 0";
+                    gameLoop(canvas, leftPaddle, rightPaddle, upPaddle, downPaddle, ball, gameState);
+                    startCountdown(gameState, ball);
+                }
+            }
+        });
+    })
+}
